6440247e1a3a85266477b39749703d59593d1285
LIM JANGBIN <great_jb2005@naver.com>
2025-08-29 11:10:32 +0900

    Update colors.js

--- colors.js
+++ colors.js
@@ -1,1 +1,33 @@
-//h
+  
+  var Links = {
+    setColor(color)  {
+      var alist = document.querySelectorAll('a') ;
+      var i =0 ;
+      while(i<alist.length) {
+      alist[i].style.color=color;
+      i=i+1;
+      }
+    }
+  }
+  var Body = {
+    setColor(color)  {
+      document.querySelector('body').style.color = color ;
+    },
+  }
+    
+  function nightDayHandler(self) {
+    var target = document.querySelector('body') ;
+    if (self.value === 'night') {
+      target.style.backgroundColor='black' ;
+      Body.setColor('white'); 
+      self.value='day' ;
+      
+      Links.setColor('powderblue') ;
+    } else {
+      target.style.backgroundColor='white' ;
+      Body.setColor('black');
+      self.value='night' ;
+      
+      Links.setColor('blue');
+    }
+  }
