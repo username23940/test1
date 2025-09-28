91c8def3f16af4de668da7f06dd371429caf38d2
LIM JANGBIN <great_jb2005@naver.com>
2025-09-28 22:26:03 +0900

    Update syntax.js

--- syntax/syntax.js
+++ syntax/syntax.js
@@ -1,1 +1,11 @@
 console.log(1+1);
+
+console.log("1"+"1");
+
+var a=1;
+console.log(a);
+
+var name = kkk ;
+var letter = "Dear " + name + ", Nice\nto meet you."
+var letterprime = `Dear ${name}, Nice
+  to meet you.`
