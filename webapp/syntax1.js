2a63439c4aa496dc664d1fee62b424ac16001e15
LIM JANGBIN <great_jb2005@naver.com>
2025-09-28 23:02:39 +0900

    Update and rename main.js to webapp/syntax1.js

---
+++ webapp/syntax1.js
@@ -0,0 +1,22 @@
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+// module
+
+var app = http.createServer(function(request,response){
+    var _url = request.url;
+    var queryData = url.parse(request.url, true).query ; // url 입력 시 query string 부분 변수 저장
+    console.log(queryData.id) 
+    if(_url == '/'){
+      _url = '/index.html';
+    }
+    if(_url == '/favicon.ico'){
+      response.writeHead(404);
+      response.end();
+      return;
+    }
+    response.writeHead(200);
+    response.end(fs.readFileSync(queryData.id)); // querystring 그대로 출력
+ 
+});
+app.listen(3000);
