4ca8b0c7e47d17a690c10a496420951f4ed7e108
LIM JANGBIN <great_jb2005@naver.com>
2025-09-28 23:51:39 +0900

    Update syntax3.js

--- webapp/syntax3.js
+++ webapp/syntax3.js
@@ -1,5 +1,5 @@
 var fs = require("fs");
-fs.readFile("sample.txt", (err, data) => {
+fs.readFile("sample.txt", 'utf8', (err, data) => { // utf8 해야 인코딩? 가능
 	if (err) throw err;
-	console.log(data);
+	console.log(data); // 실행할 때 webapp dir에서 실행해야 함. 상위 디렉토리에서 하면 sample.txt를 못찾음
 });
