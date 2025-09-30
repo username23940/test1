f3971e4432fbe9d1228ceb6038add1f485878136
LIM JANGBIN <great_jb2005@naver.com>
2025-09-30 18:59:18 +0900

    Update syntax5(func).js

--- webapp/syntax5(func).js
+++ webapp/syntax5(func).js
@@ -21,3 +21,27 @@ function f123(){
   console.log(3);
   console.log(4);
 }
+
+
+
+
+console.log(Math.round(1.6)); //2
+console.log(Math.round(1.4)); //1
+// object.method(parameter)
+ 
+function sum(first, second){ // parameter
+  console.log(first+second);
+}
+ 
+sum(2,4); // argument
+
+
+
+console.log(Math.round(1.6)); //2
+console.log(Math.round(1.4)); //1
+ 
+function sum(first, second){ // parameter
+  return first+second; // return(바로 출력 X)
+}
+ 
+console.log(sum(2,4)); // argument
