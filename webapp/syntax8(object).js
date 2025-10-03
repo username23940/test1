e8b0887b671c5cafad2924da39f731f76bc4d70f
LIM JANGBIN <great_jb2005@naver.com>
2025-10-03 18:12:42 +0900

    Create syntax8(object).js

---
+++ webapp/syntax8(object).js
@@ -0,0 +1,9 @@
+var members = ['egoing', 'k8805', 'hoya']; // arr
+console.log(members[1]); // k8805
+ 
+var roles = { // obj
+  'programmer':'egoing',
+  'designer' : 'k8805',
+  'manager' : 'hoya'
+}
+console.log(roles.designer); //k8805
