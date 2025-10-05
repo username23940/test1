076581a70d29679023f1871c6d95e4af44cf5aed
LIM JANGBIN <great_jb2005@naver.com>
2025-10-05 19:41:51 +0900

    Update syntax10(module1).js

--- webapp/syntax10(module1).js
+++ webapp/syntax10(module1).js
@@ -5,7 +5,4 @@ var M = {
   }
 }
 
-module.exports = M; 
-
-// 우리가 만드는 모듈인 이 파일의 여러 기능 중 M이라는 객체만 바깥에서 사용할 수 있도록 export
-// module을 가져올 때는 var mod = require('./syntax10(module1).js') -> console.log(mod) 시 v, f 출력. mod.f();로 호출
+module.exports = M; // 우리가 만드는 모듈인 이 파일의 여러 기능 중 M이라는 객체만 바깥에서 사용할 수 있도록 export
